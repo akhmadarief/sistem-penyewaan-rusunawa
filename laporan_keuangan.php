17e6eb05db5a4d79f28861140ac8c5d97082c28b
rizqialfani01 <rizqialfani01@gmail.com>
2020-01-25 11:20:24 +0700

    Menambah Semua File Halaman

--- laporan_keuangan.php
+++ laporan_keuangan.php
@@ -14,12 +14,12 @@
         <div class="content-wrapper">
             <!-- START PAGE CONTENT-->
             <div class="page-heading">
-                <h1 class="page-title">Pilih Kamar</h1>
+                <h1 class="page-title">Laporan Keuangan</h1>
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item">
                         <a href="index.html"><i class="la la-home font-20"></i></a>
                     </li>
-                    <li class="breadcrumb-item">Pilih Kamar</li>
+                    <li class="breadcrumb-item">Laporan Keuangan</li>
                 </ol>
             </div>
             <div class="page-content fade-in-up">
@@ -27,35 +27,7 @@
                     <div class="col-md-12">
                         <div class="ibox">
                             <div class="ibox-head">
-                                <div class="ibox-title">Denah Kamar</div>
-                                <div class="ibox-tools">
-                                    <a class="ibox-collapse"><i class="fa fa-minus"></i></a>
-                                </div>
-                            </div>
-                            <div class="ibox-body">
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="ibox">
-                            <div class="ibox-head">
-                                <div class="ibox-title">Data Penghuni I</div>
-                                <div class="ibox-tools">
-                                    <a class="ibox-collapse"><i class="fa fa-minus"></i></a>
-                                </div>
-                            </div>
-                            <div class="ibox-body">
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="ibox">
-                            <div class="ibox-head">
-                                <div class="ibox-title">Data Penghuni II</div>
+                                <div class="ibox-title">Data Laporan Keuangan</div>
                                 <div class="ibox-tools">
                                     <a class="ibox-collapse"><i class="fa fa-minus"></i></a>
                                 </div>
