1ad46a85519363d6e1cc1a134146b7b08e789711
akhmadarief <38371717+akhmadarief@users.noreply.github.com>
2020-01-25 11:34:10 +0700

    require config & css (replace head)

--- daftar_kamar.php
+++ daftar_kamar.php
@@ -1,7 +1,17 @@
+<?php require_once '_partials/config.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
-<?php require_once '_partials/head.php' ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>Sistem Penyewaan Rusunawa | Beranda</title>
+
+    <!-- CSS -->
+    <?php require_once '_partials/css.php' ?>
+</head>
 
 <body class="fixed-navbar">
     <div class="page-wrapper">
--- daftar_penghuni.php
+++ daftar_penghuni.php
@@ -7,7 +7,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width initial-scale=1.0">
-    <title>Sistem Penyewaan Rusunawa | Daftar Penghuni</title>
+    <title>Sistem Penyewaan Rusunawa | Beranda</title>
+
+    <!-- CSS -->
     <?php require_once '_partials/css.php' ?>
 </head>
 
