a287ed6640876734c4dfd6d132962939e0ec235c
rizqialfani01 <rizqialfani01@gmail.com>
2020-01-25 15:44:13 +0700

    Daftar Kamar, Harga, Penghuni

--- daftar_harga.php
+++ daftar_harga.php
@@ -37,7 +37,42 @@
                                 </div>
                             </div>
                             <div class="ibox-body">
-                                
+                                <table class="table table-striped table-bordered table-hover" id="tabel-penghuni" cellspacing="0" width="100%">
+                                    <thead>
+                                        <tr>
+                                            <th class='text-center'>No.</th>
+                                            <th class='text-center'>Gedung</th>
+                                            <th class='text-center'>Lantai</th>
+                                            <th class='text-center'>Harga per Bulan</th>
+                                            <th class='text-center'>Harga per Tahun</th>
+                                            <th class='text-center'>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $no = 1;
+                                            //$harga = $conn->query("SELECT kamar.gedung, harga.lantai, harga.harga FROM harga OUTER JOIN kamar ON harga.lantai=kamar.lantai");
+                                            $harga = $conn->query("SELECT * FROM harga");
+                                            while ($row = $harga->fetch_assoc()) {
+                                                echo
+                                                "<tr>
+                                                    <td class='text-center'>".$no++."</td>
+                                                    <td class='text-center'>Gedung ".$row['gedung']."</td>
+                                                    <td class='text-center'>Lantai ".$row['lantai']."</td>
+                                                    <td class='text-center'>Rp. ".number_format($row['harga'], 0, ',', '.')."</td>
+                                                    <td class='text-center'>Rp. ".number_format($row['harga']*12, 0, ',', '.')."</td>
+                                                    <td class='text-center'>
+                                                        <button class='btn btn-sm btn-info edit_harga' id='".$row['lantai']."'>
+                                                            <span class='ti-pencil'>
+                                                            </span>Edit Harga
+                                                        </button>
+                                                    </td>
+                                                </tr>";
+
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -55,6 +90,26 @@
     <!-- END PAGA BACKDROPS-->
     <!-- CORE PLUGINS-->
     <?php require_once '_partials/js.php' ?>
+
+    <!-- CORE PLUGINS & SCRIPTS-->
+    <?php require_once '_partials/js.php' ?>
+
+    <!-- PAGE LEVEL SCRIPTS-->
+    <script type="text/javascript">
+        $(function() {
+            $('#tabel-penghuni').DataTable({
+                pageLength: 10,
+                //"ajax": './assets/demo/data/table_data.json',
+                /*"columns": [
+                    { "data": "name" },
+                    { "data": "office" },
+                    { "data": "extn" },
+                    { "data": "start_date" },
+                    { "data": "salary" }
+                ]*/
+            });
+        })
+    </script>
 
 </body>
 
