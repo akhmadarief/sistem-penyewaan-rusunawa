198b49e14d9a39b78d6e3324cd68a364442bac32
rizqialfani01 <rizqialfani01@gmail.com>
2020-01-25 14:34:04 +0700

    Pilih Kamar

--- _partials/css.php
+++ _partials/css.php
@@ -8,3 +8,5 @@
     <link href="./assets/vendors/DataTables/datatables.min.css" rel="stylesheet" />
     <!-- THEME STYLES-->
     <link href="assets/css/main.min.css" rel="stylesheet" />
+    <!-- CUSTOM STYLES-->
+    <link type="text/css" href="assets/css/custom.css" rel="stylesheet" />
