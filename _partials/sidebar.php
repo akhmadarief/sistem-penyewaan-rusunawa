13bf571828182491604e1775be4c8503eb6dffce
rizqialfani01 <rizqialfani01@gmail.com>
2020-01-25 11:41:39 +0700

    Logo Sidebar

--- _partials/sidebar.php
+++ _partials/sidebar.php
@@ -9,37 +9,37 @@
                 </div>
                 <ul class="side-menu metismenu">
                     <li>
-                        <a class="active" href="./"><i class="sidebar-item-icon fa fa-th-large"></i>
-                            <span class="nav-label">Beranda</span>
+                        <a class="active" href="./"><i class="sidebar-item-icon fa fa-dashboard"></i>
+                            <span class="nav-label">Dasbor</span>
                         </a>
                     </li>
                     <li>
-                        <a href="kamar.php"><i class="sidebar-item-icon fa fa-th-large"></i>
+                        <a href="kamar.php"><i class="sidebar-item-icon fa fa-th"></i>
                             <span class="nav-label">Pilih Kamar</span>
                         </a>
                     </li>
                     <li>
-                        <a href="daftar_kamar.php"><i class="sidebar-item-icon fa fa-th-large"></i>
+                        <a href="daftar_kamar.php"><i class="sidebar-item-icon fa fa-th-list"></i>
                             <span class="nav-label">Daftar Kamar</span>
                         </a>
                     </li>
                     <li>
-                        <a href="daftar_harga.php"><i class="sidebar-item-icon fa fa-th-large"></i>
+                        <a href="daftar_harga.php"><i class="sidebar-item-icon fa fa-dollar"></i>
                             <span class="nav-label">Daftar Harga Kamar</span>
                         </a>
                     </li>
                     <li>
-                        <a href="daftar_penghuni.php"><i class="sidebar-item-icon fa fa-th-large"></i>
+                        <a href="daftar_penghuni.php"><i class="sidebar-item-icon fa fa-users"></i>
                             <span class="nav-label">Daftar Penghuni</span>
                         </a>
                     </li>
                     <li>
-                        <a href="laporan_keuangan.php"><i class="sidebar-item-icon fa fa-th-large"></i>
+                        <a href="laporan_keuangan.php"><i class="sidebar-item-icon fa fa-money"></i>
                             <span class="nav-label">Laporan Keuangan</span>
                         </a>
                     </li>
                     <li>
-                        <a href="laporan_piutang.php"><i class="sidebar-item-icon fa fa-th-large"></i>
+                        <a href="laporan_piutang.php"><i class="sidebar-item-icon fa fa-credit-card-alt"></i>
                             <span class="nav-label">Laporan Piutang</span>
                         </a>
                     </li>
