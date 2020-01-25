fc85bf2b73b88cbeecaffdc7740a82976f571143
rizqialfani01 <rizqialfani01@gmail.com>
2020-01-25 21:53:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <!-- START PAGE CONTENT-->
             <div class="page-content fade-in-up">
                 <div class="row">
-                    <div class="col-lg-3 col-md-6">
+                    <div class="col-lg-4 col-md-6">
                         <div class="ibox bg-success color-white widget-stat">
                             <div class="ibox-body">
                                 <h2 class="m-b-5 font-strong">201</h2>
@@ -28,7 +28,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6">
+                    <div class="col-lg-4 col-md-6">
                         <div class="ibox bg-info color-white widget-stat">
                             <div class="ibox-body">
                                 <h2 class="m-b-5 font-strong">1250</h2>
@@ -37,7 +37,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6">
+                    <div class="col-lg-4 col-md-6">
                         <div class="ibox bg-warning color-white widget-stat">
                             <div class="ibox-body">
                                 <h2 class="m-b-5 font-strong">$1570</h2>
@@ -46,7 +46,25 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="ibox bg-danger color-white widget-stat">
+                            <div class="ibox-body">
+                                <h2 class="m-b-5 font-strong">108</h2>
+                                <div class="m-b-5">NEW USERS</div><i class="ti-user widget-stat-icon"></i>
+                                <div><i class="fa fa-level-down m-r-5"></i><small>-12% Lower</small></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="ibox bg-danger color-white widget-stat">
+                            <div class="ibox-body">
+                                <h2 class="m-b-5 font-strong">108</h2>
+                                <div class="m-b-5">NEW USERS</div><i class="ti-user widget-stat-icon"></i>
+                                <div><i class="fa fa-level-down m-r-5"></i><small>-12% Lower</small></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
                         <div class="ibox bg-danger color-white widget-stat">
                             <div class="ibox-body">
                                 <h2 class="m-b-5 font-strong">108</h2>
@@ -526,38 +544,6 @@
                     }
                 </style>
                 <div>
-                    <a class="adminca-banner" href="http://admincast.com/adminca/" target="_blank">
-                        <div class="adminca-banner-ribbon"><i class="fa fa-trophy mr-2"></i>PREMIUM TEMPLATE</div>
-                        <div class="wrap-1">
-                            <div class="wrap-2">
-                                <div>
-                                    <img src="./assets/img/adminca-banner/adminca-preview.jpg" style="height:160px;margin-top:50px;" />
-                                </div>
-                                <div class="color-white" style="margin-left:40px;">
-                                    <h1 class="font-bold">ADMINCA</h1>
-                                    <p class="font-16">Save your time, choose the best</p>
-                                    <ul class="list-unstyled">
-                                        <li class="m-b-5"><i class="ti-check m-r-5"></i>High Quality Design</li>
-                                        <li class="m-b-5"><i class="ti-check m-r-5"></i>Fully Customizable and Easy Code</li>
-                                        <li class="m-b-5"><i class="ti-check m-r-5"></i>Bootstrap 4 and Angular 5+</li>
-                                        <li class="m-b-5"><i class="ti-check m-r-5"></i>Best Build Tools: Gulp, SaSS, Pug...</li>
-                                        <li><i class="ti-check m-r-5"></i>More layouts, pages, components</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div style="flex:1;">
-                                <div class="d-flex justify-content-end wrap-3">
-                                    <div class="adminca-banner-b m-r-20">
-                                        <img src="./assets/img/adminca-banner/bootstrap.png" style="width:40px;margin-right:10px;" />Bootstrap v4</div>
-                                    <div class="adminca-banner-b m-r-10">
-                                        <img src="./assets/img/adminca-banner/angular.png" style="width:35px;margin-right:10px;" />Angular v5+</div>
-                                </div>
-                                <div class="dev-img">
-                                    <img src="./assets/img/adminca-banner/sprite.png" />
-                                </div>
-                            </div>
-                        </div>
-                    </a>
                 </div>
             </div>
             <!-- END PAGE CONTENT-->
